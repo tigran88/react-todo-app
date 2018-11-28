c8db1a9b292083e134b6bef8902de332f3b63d8a
Tigran <tigransimonyan88@gmail.com>
2018-11-28 17:30:03 +0400

    remove <BrowserRouter> in App.js add it in index.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 import Layout from './containers/Layout/Layout';
@@ -7,9 +6,7 @@ import Layout from './containers/Layout/Layout';
 class App extends Component {
   render() {
     return (
-        <BrowserRouter>
-          <Layout />
-        </BrowserRouter>
+        <Layout />
     );
   }
 }
