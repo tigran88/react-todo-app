a5c8959b8ad72f0066c40ba28d870ff783708339
Tigran <tigransimonyan88@gmail.com>
2018-11-30 12:36:09 +0400

    move route setup from Layout.js to App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Layout from './containers/Layout/Layout';
 
+const Home = lazy(() => import('./containers/Home/Home'));
+const Todos = lazy(() => import('./containers/Todos/Todos'));
+const ErrorNotFound = lazy(() => import('./components/ErrorNotFound/ErrorNotFound'));
+
 class App extends Component {
   render() {
     return (
-        <Layout />
+        <Layout>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path='/' component={ Home } exact/>
+                    <Route path='/todos' component={ Todos } />
+                    <Route component={ ErrorNotFound } />
+                </Switch>
+            </Suspense>
+        </Layout>
     );
   }
 }
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,29 +1,20 @@
-import React, { lazy, Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
 
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 
-const Home = lazy(() => import('../Home/Home'));
-const Todos = lazy(() => import('../Todos/Todos'));
-const ErrorNotFound = lazy(() => import('../../components/ErrorNotFound/ErrorNotFound'));
+class Layout extends Component {
+    render() {
+        return (
+            <>
+                <Header />
+                <main>
+                    {this.props.children}
+                </main>
+                <Footer />
+            </>
+        )
+    }
+}
 
-const layout = () => {
-    return (
-        <>
-            <Header />
-            <main>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Switch>
-                        <Route path='/' component={ Home } exact/>
-                        <Route path='/todos' component={ Todos } />
-                        <Route component={ErrorNotFound} />
-                    </Switch>
-                </Suspense>
-            </main>
-            <Footer />
-        </>
-    )
-};
-
-export default layout;
+export default Layout;
